b6d8cd77f94c38267e89027575f9a890927671b5
ArjixGamer <53124886+ArjixGamer@users.noreply.github.com>
2021-06-24 10:43:38 +0300

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { eel } from "./components/eel.js";
 import "./App.css";
+import "./assets/main.css";
 
 import NavigationBar from "./components/NavBar/Nav";
 import SearchElement from "./components/SearchPage/Search";
